a29aacd2e881190d19838e90b14819a41338bd58
Frederik Holfeld <frederik.holfeld@gmail.com>
2023-05-22 01:25:04 +0200

    Refine guessing game
    As a bit of practice and getting into the language again, I'm poking around in the guessing game and make it a bit nicer. Using loops in variable assignment statements seems pretty crazy and cool to me right now :).

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,36 +1,43 @@
-use std::io;
+use std::io::stdin;
 use rand::Rng;
-use std::cmp::Ordering;
+use std::cmp::Ordering::{Less, Greater, Equal};
 
 fn main() {
     println!("We're going to play a guessing game!");
-
     println!("Please set the maximum value (not higher than 255):");
-    let mut max = String::new();
-    io::stdin()
-        .read_line(&mut max)
-        .expect("Failed to read user input!");
-    let max: u8 = max.trim().parse().expect("Type a number smaller than or equal to 255!");
+    
+    let max: u8 = loop {
+        let mut max = String::new();
+        stdin()
+            .read_line(&mut max)
+            .expect("Failed to read user input!");
+        match max.trim().parse() {
+            Ok(num) => break num,
+            Err(_)  => println!("You need to enter a number smaller than 255!")
+        }
+    };
 
-    let secret_number = rand::thread_rng().gen_range(1..=max);
-
-    println!("Now guess the right number between 1 and {max}!");
+    let secret_number = rand::thread_rng().gen_range(0..=max);
+    println!("Now guess the right number between 0 and {max}!");
 
     loop {
         println!("Type your guess:");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read user input!");
-        let guess: u8 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue
+        
+        let guess: u8 = loop {
+            let mut guess = String::new();
+            stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read user input!");
+            match guess.trim().parse() {
+                Ok(num) => break num,
+                Err(_)  => println!("You need to enter a number between 0 and {max}!")
+            }
         };
-
+        
         match guess.cmp(&secret_number) {
-            Ordering::Less      => println!("You guessed too low!"),
-            Ordering::Greater   => println!("You guessed too high!"),
-            Ordering::Equal     => {
+            Less    => println!("You guessed too low!"),
+            Greater => println!("You guessed too high!"),
+            Equal   => {
                 println!("You guessed correctly! GG :)");
                 break;
             }
